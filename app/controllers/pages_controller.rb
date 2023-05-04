9ac4454c88ee8e0b1b87bba109b9e82a08e53b08
Sebastiano Labbate <seb.94@hotmail.co.uk>
2023-05-04 17:51:03 +0100

    map fixes and gallery creation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @markers =
+      [{
+        lat: 52.9381732751361652,
+        lng: -1.1404715734755337,
+        info_window_html: render_to_string(partial: "info_window")
+      }]
   end
 end
