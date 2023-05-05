5f55f15eaf32c50c7702808b1675f4e83b0444d1
Sebastiano Labbate <seb.94@hotmail.co.uk>
2023-05-05 14:58:12 +0100

    converted vanilla js to stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import GalleryController from "./gallery_controller"
+application.register("gallery", GalleryController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
