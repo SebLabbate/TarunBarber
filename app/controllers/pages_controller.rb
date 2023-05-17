2698c0e0b026cef0c7662c83c56b14e6b014cd0a
Sebastiano Labbate <seb.94@hotmail.co.uk>
2023-05-17 15:20:04 +0100

    basic contact us page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :tarun ]
+  skip_before_action :authenticate_user!, only: %I[home tarun contact]
 
   def home
     @markers =
@@ -11,6 +11,14 @@ class PagesController < ApplicationController
   end
 
   def tarun
+  end
 
+  def contact
+    @markers =
+      [{
+        lat: 52.9381732751361652,
+        lng: -1.1404715734755337,
+        info_window_html: render_to_string(partial: "info_window")
+      }]
   end
 end
